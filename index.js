ec29dc9309bfe5267843004813993e51506157b3
stella-mumbi <stella.mumbi@student.moringaschool.com>
2023-12-12 15:22:19 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file!
+
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood = 'Chelsea'
+let actualNeighborhood = mostProfitableNeighborhood.toLowerCase();
+
+
+let companyCeo = "Susan Smith"
+let realCompanyCeo = companyCeo.toLowerCase();
+
+
